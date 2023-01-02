00370cab8ff68bf36e99aa39fbbe04afeff0164c
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2023-01-02 17:57:17 +0200

    update 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const CURRENT_TIME_PLAYER = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+    localStorage.setItem(CURRENT_TIME_PLAYER, JSON.stringify(data.seconds));
+};
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_PLAYER)) || 0);
+
 
