6007e464df750c5abcb0f52301b6b655b27b0afb
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2023-01-02 16:17:58 +0200

    update 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarckup = makeGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarckup);
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    })
+    .join(' ');
+}
+
+const modal = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
