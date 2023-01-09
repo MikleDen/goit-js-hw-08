ccfa1e4db0177a1489e9aed204168b1244956466
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2023-01-09 11:35:47 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,50 +1,45 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('input'),
-  submit:document.querySelector('button[type="submit"]')
-};
+const refForm =document.querySelector('.feedback-form');
+
+refForm.addEventListener('submit', onSubmit);
+refForm.addEventListener('input', throttle(onTextareaInput, 500));
+
+setInputValue();
 const formData = {};
 
-
-
-// function onInput(e){
-//     const inputValue= e.target.value;
-//     console.log(inputValue);
-// }
-
-refs.form.addEventListener('submit', onSubmit);
 function onSubmit (e) {
     e.preventDefault();
     e.currentTarget.reset();
-    const objData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    formData.email = refForm.elements.email.value;
+    formData.message = refForm.elements.message.value;
+    
     localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
   }
 
-
-  
-
-refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 function onTextareaInput(e) {
   formData[e.target.name] = e.target.value;
   const strData = JSON.stringify(formData);
   localStorage.setItem(STORAGE_KEY, strData);
 }
-setInputValue();
+
 
 function setInputValue() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedMessage === null) {
-    //console.log(savedMessage);
-    return;
+  const savedForm = localStorage.getItem(STORAGE_KEY);
+  if (savedForm) {
+      const parceSavedForm = JSON.parse(savedForm);
+      // console.log(parceSavedForm);
+      for (const prop in parceSavedForm) {
+      if (parceSavedForm.hasOwnProperty(prop)) {
+          // console.log(parceSavedForm[prop]);
+          refForm.elements[prop].value = parceSavedForm[prop];
+          formData[prop] = parceSavedForm[prop];
+      }
   }
-  refs.textarea.value = savedMessage['message'] || '';
-  refs.input.value = savedMessage['email'] || '';
+  }  
 }
 
 
