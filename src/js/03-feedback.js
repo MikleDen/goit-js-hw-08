2f52501351c9a466b50aafff7a5aabb60cb5708d
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2023-01-09 14:40:59 +0200

    correct 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,44 +2,42 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 const refForm =document.querySelector('.feedback-form');
+const input = document.querySelector('[name="email"]');
+const textArea = document.querySelector('[name="message"]');
 
 refForm.addEventListener('submit', onSubmit);
 refForm.addEventListener('input', throttle(onTextareaInput, 500));
 
 setInputValue();
-const formData = {};
+const dataUser = { [input.name]: input.value, [textArea.name]: textArea.value };
+localStorage.setItem(STORAGE_KEY, JSON.stringify(dataUser));
+
 
 function onSubmit (e) {
     e.preventDefault();
-    e.currentTarget.reset();
-    formData.email = refForm.elements.email.value;
-    formData.message = refForm.elements.message.value;
-    
+    e.target.reset();
+   
     localStorage.removeItem(STORAGE_KEY);
-    console.log(formData);
+    console.log(dataUser);
   }
 
 
 function onTextareaInput(e) {
-  formData[e.target.name] = e.target.value;
-  const strData = JSON.stringify(formData);
+  dataUser[input.name] = input.value;
+  dataUser[textArea.name] = textArea.value;
+  const strData = JSON.stringify(dataUser);
   localStorage.setItem(STORAGE_KEY, strData);
 }
 
 
 function setInputValue() {
-  const savedForm = localStorage.getItem(STORAGE_KEY);
-  if (savedForm) {
-      const parceSavedForm = JSON.parse(savedForm);
-      // console.log(parceSavedForm);
-      for (const prop in parceSavedForm) {
-      if (parceSavedForm.hasOwnProperty(prop)) {
-          // console.log(parceSavedForm[prop]);
-          refForm.elements[prop].value = parceSavedForm[prop];
-          formData[prop] = parceSavedForm[prop];
-      }
+  const savedMsg = localStorage.getItem(STORAGE_KEY);
+  const currentDataUser = JSON.parse(savedMsg);
+  if (!currentDataUser) {
+    return;
   }
-  }  
+  input.value = currentDataUser.email;
+  textArea.value = currentDataUser.message;
 }
 
 
